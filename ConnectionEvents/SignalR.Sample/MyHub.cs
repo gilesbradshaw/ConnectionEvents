a869ca3419bb4a7cdf20fb51fa10f812aaa83f20
GilesBradshaw <giles.bradshaw@sigyl.com>
2012-07-03 09:21:21 +0100

    initial commit

---
+++ ConnectionEvents/SignalR.Sample/MyHub.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using SignalR.Hubs;
+
+namespace SignalR
+{
+    [HubName("myHub")]
+    public class MyHub : Hub, IDisconnect, IConnected
+    {
+        
+        
+        public string GetString()
+        {
+            System.Diagnostics.Debug.WriteLine(Context.ConnectionId);
+            return "hello";
+        }
+
+        private static int y = 0;
+        private static int x = 0;
+        public System.Threading.Tasks.Task Disconnect()
+        {
+            x = x - 1;
+            System.Diagnostics.Debug.WriteLine("disconnecting: " + x.ToString() + "  : " + y++.ToString());
+            return new Task(() => { });
+
+        }
+
+        public System.Threading.Tasks.Task Connect()
+        {
+            x = x + 1;
+            System.Diagnostics.Debug.WriteLine("connecting: " + x.ToString());
+            return new Task(() => { });
+        }
+
+        public System.Threading.Tasks.Task Reconnect(IEnumerable<string> groups)
+        {
+            return new Task(() => { });
+        }
+    }
+}
---
+++ ConnectionEvents/SignalR.Sample/SignalR.js
@@ -0,0 +1,33 @@
+﻿    /// <reference path="../scripts/jquery-1.7.2.js" />
+    /// <reference path="../scripts/jquery.signalR-0.5.2.js" />
+
+
+
+$(function () {
+
+    var ticker = $.connection.myHub;
+
+    function init() {
+        return ticker.getString().done(function (aString) {
+            
+        });
+    }
+
+    // Add client-side hub methods that the server will call
+    ticker.Anyoldfunctionname = function () {
+
+
+    };
+
+
+
+
+    // Start the connection
+    $.connection.hub.start()
+            .done(function () {
+                return init();
+            });
+
+
+
+});
